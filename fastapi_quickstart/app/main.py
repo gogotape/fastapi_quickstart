4f1ce7ccd332f9867fc49d948e0f03739105a7e3
Mikhail <bb@mail.ru>
2023-11-25 00:29:43 +0300

    feat: add model and endpoints

--- fastapi_quickstart/app/main.py
+++ fastapi_quickstart/app/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from app.models.models import User
 
 
 app = FastAPI()
+user1 = User(name="John Doe", age=15)
 
 
 @app.get("/")
@@ -11,8 +13,20 @@ async def root():
 
 
 @app.get("/custom")
-def read_custom_message():
+async def read_custom_message():
     return {"message": "This is a custom message!"}
+
+
+@app.get("/users", response_model=User)
+async def users():
+    return user1
+
+
+@app.post("/user")
+async def is_user_adult(user: User):
+    return {"name": user.name,
+            "age": user.age,
+            "is_adult": user.age >= 18}
 
 
 if __name__ == "__main__":
