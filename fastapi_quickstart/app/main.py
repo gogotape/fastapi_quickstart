a7c620d4f98f5d53727982f73e1efd722b74f858
Mikhail <bb@mail.ru>
2023-11-28 01:48:57 +0300

    feat: add new models and end-points
    # lesson 3.1

--- fastapi_quickstart/app/main.py
+++ fastapi_quickstart/app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from app.models.models import User
+from app.models.models import User, UserCreate, Product
+from app.data.db import sample_products
 
 
 app = FastAPI()
@@ -27,6 +28,22 @@ async def is_user_adult(user: User):
     return {"name": user.name,
             "age": user.age,
             "is_adult": user.age >= 18}
+
+
+@app.post("/create_user")
+async def create_user(user: UserCreate):
+    return user
+
+
+@app.get("/product/{product_id}")
+async def get_product(product_id: int) -> Product:
+    product = [item for item in sample_products if item['product_id'] == product_id]
+    return product[0]
+
+
+@app.get("/products/search")
+async def search_product(keyword: str, category: str = None, limit: int = 10):
+    return list(filter(lambda x: x["category"] == category and keyword in x["name"], sample_products[:limit]))
 
 
 if __name__ == "__main__":
--- fastapi_quickstart/app/models/models.py
+++ fastapi_quickstart/app/models/models.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt, EmailStr
 
 
 class User(BaseModel):
     name: str
     age: int
+
+
+class UserCreate(BaseModel):
+    name: str
+    age: PositiveInt = None
+    email: EmailStr
+    is_subscribed: bool = None
+
+
+class Product(BaseModel):
+    product_id: int
+    name: str
+    category: str
+    price: float
